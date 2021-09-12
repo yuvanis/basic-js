4214ce261e3754bd358571778fd382de7ef9a60f
yuvanis <75557443+yuvanis@users.noreply.github.com>
2021-09-12 12:16:29 +0300

    feat: add code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity != "string" ||
+    sampleActivity < 0 || MODERN_ACTIVITY < sampleActivity) {
+    return false;
+  }
+  let active = Number(sampleActivity);
+  if (!active) {
+    return false;
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) * (HALF_LIFE_PERIOD / Math.LN2));
 }
