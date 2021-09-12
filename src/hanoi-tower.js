05a63293ff29c566d1706a8d1ca7f02960fb1124
yuvanis <75557443+yuvanis@users.noreply.github.com>
2021-09-12 12:18:49 +0300

    faet: add code

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,8 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = Math.pow(2, disksNumber) - 1;
+  let sec = Math.floor(turns / (turnsSpeed / 3600));
+  return { turns: turns, seconds: sec };
 }
