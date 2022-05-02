4d6281a3a0ae89946d4f06f63a32ccb03f863baf
yuvanis <uvnistratov@gmail.com>
2022-05-02 21:17:27 +0300

    feat: add js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  isReverse;
+
+  constructor(isReverse = false) {
+    this.isReverse = isReverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  makeKey(key, length) {
+    let result = key;
+    while(result.length < length)
+      result += key;
+    return result.toUpperCase();
+  }
+
+  reverse(str){
+    let result = '';
+    for (let index = str.length - 1; index >= 0 ; index--) {
+      const element = str[index];
+      result += element;
+    }
+    return result;
+  }
+
+  crypt(m, k, isEncrypt) {
+    if(m === undefined || k == undefined)
+      throw new Error( 'Incorrect arguments!')
+    const message = this.isReverse ? this.reverse(m.toUpperCase()) : m.toUpperCase();
+    const key = this.makeKey(k, message.length);
+    let result = '';
+    for(let i = 0, j = 0; i < message.length; i++, j++) {
+      const mLetterIndex = this.alphabet.indexOf(message[i]);
+      if(mLetterIndex === -1) {
+        result += message[i];
+        j--;
+        continue;
+      }
+      const kLetterIndex = this.alphabet.indexOf(key[j]);
+      let newIndex;
+      if(isEncrypt)
+        newIndex = (kLetterIndex + mLetterIndex) % this.alphabet.length;
+      else 
+        newIndex = Math.abs(this.alphabet.length - kLetterIndex + mLetterIndex) % this.alphabet.length;
+      result += this.alphabet[newIndex];
+    }
+    return !isEncrypt && this.isReverse ? this.reverse(result) : result;
+  }
+
+  encrypt(m, k) {
+    return this.crypt(m, k, true);
+  }
+  decrypt(m, k) {
+    return this.crypt(m, k, false);
   }
 }
 
